bda89aad137d1b5b9ad0b64c073f171b7eb93eeb
Osiris Mariano <oslimacontato@gmail.com>
2023-04-19 09:45:21 -0300

    Atualiza bubble_sort.rb, maximize_array.rb

--- fase_01/bubble_sort.rb
+++ fase_01/bubble_sort.rb
@@ -1,34 +1,21 @@
-=begin 
+def bubblesort(arry, length)
+  sorted = true
 
-O Bubble Sort é o algoritmo de classificação mais simples que funciona 
-trocando repetidamente os elementos adjacentes se eles estiverem na ordem errada. 
-Este algoritmo não é adequado para grandes conjuntos de dados, pois sua complexidade 
-de tempo média e de pior caso é bastante alta.
-
-O Bubble Sort é o algoritmo de classificação mais simples que funciona
-trocando repetidamente os elementos adjacentes se eles estiverem na ordem errada.
-Este algoritmo não é adequado para grandes conjuntos de dados, pois sua complexidade
-de tempo médio e de pior caso é bastante alto.
-
-link: https://www.geeksforgeeks.org/bubble-sort/
-
-=end
-
-def bubble_sort(lista)
-  comprimento = lista.length
-  loop do
-    trocado = false
-    (comprimento-1).times do|i|
-      if
-        lista[i] > lista[i+1]
-        lista[i], lista[i+1] = lista[i+1], lista[i]
-        trocado = true
-      end
+  for i in 0..(length - 2)
+    if array [i] > arry[i + 1]
+      arry[i], array[i + 1] = array[i + 1], array[i]
+      sorted = false
     end
-    break if not trocado
   end
-  lista
+  if sorted == false
+    bubblesort(arry, length)
+  else
+    return arry
+  end 
 end
 
-a = [4, 0, 3, 2, 9]
-p bubble_sort(a)
+unsorted_arry = [6, 3, 0, 5]
+sorted_arry   = bubblesort(unsorted_arry, unsorted_arry.length)
+
+puts sorted_arry.inspect
+puts "a ordem da lista ficou: #{sorted_arry}"
